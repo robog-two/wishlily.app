d86f98abc3181157df94809a6bf82ecc06d6c49c
Sam Knight <robogamingbysam@icloud.com>
2022-04-24 23:23:24 -0500

    Try to fix old embeds (?)

--- src/routes/wishlist/[wishlistId]/[userId]/index.js
+++ src/routes/wishlist/[wishlistId]/[userId]/index.js
@@ -2,7 +2,9 @@ export async function get({ params, url }) {
   const { wishlistId, userId } = params
   const title = decodeURIComponent(url.searchParams.get('s')) ?? 'Loading...'
   return {
-    status: 302,
-    redirect: `https://wishlily.app/wishlist/${wishlistId}/${userId}/${encodeURIComponent(title)}`
+    status: 301,
+    headers: {
+      Location: `/wishlist/${wishlistId}/${userId}/${encodeURIComponent(title)}`
+    }
   };
 }
